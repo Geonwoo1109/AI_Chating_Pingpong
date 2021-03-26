0283a7986e7f1d04fced6cb58e82afe2ed81974b
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-03-27 01:32:18 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,23 +1,42 @@
 const scriptName = "핑퐁 챗봇";
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  if (msg == "챗봇") { try {
-    /*
-    var json = {};
-    json.request = {};
-    json.request.query = "안녕";
-    
-    replier.reply(JSON.stringify(json)+"\n"+JSON.stringify(json,null,4));
-    */
-    var json1 = { "request": { "query": "안녕!" } };
-    replier.reply(JSON.stringify(json1));
-    
-    var message = org.jsoup.Jsoup.connect("https://builder.pingpong.us/api/builder/605b0e30e4b078d873a52a7d/integration/v0.2/custom/a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk")
+  if (msg.startsWith("\"") && msg.endsWith("\"")) {
+    try {
+      
+      var json = {
+        "request": {
+          "query": msg.slice(1,-1)
+        }
+      };
+      
+      var message = JSON.parse(org.jsoup.Jsoup.connect("https://builder.pingpong.us/api/builder/605b0e30e4b078d873a52a7d/integration/v0.2/custom/a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk")
     .header("Authorization", "Basic a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk")
     .header("Content-Type", "application/json; charset=utf-8")
-    .data(json1, JSON.stringify(json1))
-    .ignoreContentType(true).ignoreHttpErrors(true).post().text();
-    replier.reply(message);
+    .requestBody(JSON.stringify(json))
+    .ignoreContentType(true).ignoreHttpErrors(true).post().text());
+    
+    try {
+      replier.reply(message.response.replies[0].text);
+    } catch (e) {
+      replier.reply(message.response.replies.text);
+    }
+    
+    /*
+    
+    const res = org.jsoup.Jsoup.connect('https://builder.pingpong.us/api/builder/605b0e30e4b078d873a52a7d/integration/v0.2/custom/a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk')
+.method(org.jsoup.Connection.Method.POST)
+.header('Content-Type', 'application/json; charset=UTF-8')
+.header('Authorization', "Basic a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk")
+.requestBody(JSON.stringify(json1))
+.ignoreContentType(true)
+.execute();
+
+const body = res.parse();
+const resJson = JSON.parse(body.text());
+
+replier.reply(JSON.stringify(resJson,null,4));
+    */
   } catch(e) {replier.reply(e+e.lineNumber);}
   }
   
