d25739a2f8f7b7e2288ee2c72fa4dbbf2deba28f
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-03-31 20:28:38 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -23,22 +23,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     } catch (e) {
       replier.reply(message.response.replies.text);
     }
-    
-    /*
-    
-    const res = org.jsoup.Jsoup.connect('https://builder.pingpong.us/api/builder/605b0e30e4b078d873a52a7d/integration/v0.2/custom/a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk')
-.method(org.jsoup.Connection.Method.POST)
-.header('Content-Type', 'application/json; charset=UTF-8')
-.header('Authorization', "Basic a2V5OjUzZWUzNjAxMzM3NmVjNzYwOGNkZTVmZjJlZWNiNWVk")
-.requestBody(JSON.stringify(json1))
-.ignoreContentType(true)
-.execute();
 
-const body = res.parse();
-const resJson = JSON.parse(body.text());
-
-replier.reply(JSON.stringify(resJson,null,4));
-    */
   } catch(e) {replier.reply(e+e.lineNumber);}
   }
   
