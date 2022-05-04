f80ffa3764fd408db58a66fa767d738aa2efa5aa
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2022-05-05 00:48:22 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,30 +1,32 @@
 const scriptName = "핑퐁 챗봇";
 
-const Key = ""; // Key
+const Key = "";
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  if (msg.startsWith("\"") && msg.endsWith("\"")) {
-    try {
-      
-      var json = {
-        "request": {
-          "query": msg.slice(1,-1)
-        }
-      };
-      
-      var message = JSON.parse(org.jsoup.Jsoup.connect("https://builder.pingpong.us/api/builder/605b0e30e4b078d873a52a7d/integration/v0.2/custom/"+Key)
-    .header("Authorization", "Basic "+Key)
-    .header("Content-Type", "application/json; charset=utf-8")
-    .requestBody(JSON.stringify(json))
-    .ignoreContentType(true).ignoreHttpErrors(true).post().text());
-    
-    try {
-      replier.reply(message.response.replies[0].text);
-    } catch (e) {
-      replier.reply(message.response.replies.text);
-    }
+    if (msg.length > 2 && msg.substr(0, 1) == ("\"") && msg.substr(msg.length-1, 1) == ("\"")) {
+        try {
+            
+            var json = {
+                "request": { "query": msg.slice(1,-1) }
+            };
+        
+            var message = JSON.parse(
+                org.jsoup.Jsoup.connect("https://builder.pingpong.us/api/builder/605b0e30e4b078d873a52a7d/integration/v0.2/custom/"+Key)
+                .header("Authorization", "Basic "+Key)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .requestBody(JSON.stringify(json))
+                .ignoreContentType(true).ignoreHttpErrors(true).post().text());
+            
+            try {
+                replier.reply(message.response.replies[0].text);
+            } catch (e) {
+                replier.reply(message.response.replies.text);
+            }
+        
 
-  } catch(e) {replier.reply(e+e.lineNumber);}
-  }
+        } catch(e) {
+            replier.reply(e+e.lineNumber +"122");
+        }
+    }
   
 }
